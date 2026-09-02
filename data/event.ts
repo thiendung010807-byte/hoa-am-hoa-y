36114a2ae24120077e42bd21619cfd2309082e9e
Thien Dung <thiendung010807@gmail.com>
2026-09-02 23:32:04 +0700

    HEHEEE

--- data/event.ts
+++ data/event.ts
@@ -12,7 +12,7 @@ export const event = {
   location: "Đại học Kinh tế Quốc dân",
   address: "Cánh trái nhà Văn Hóa - Đại học Kinh tế Quốc dân",
   mapsUrl: "https://maps.app.goo.gl/aY4K5kcDVjaUk9b78",
-  mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254.9298268680794!2d105.84487874256105!3d20.999656294540028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac71752d8f79%3A0xd2ec575c01017afa!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBLaW5oIFThur8gUXXhu5FjIETDom4gKE5FVSk!5e1!3m2!1svi!2s!4v1788366465649!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin",
+  mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254.9298268680794!2d105.84487874256105!3d20.999656294540028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac71752d8f79%3A0xd2ec575c01017afa!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBLaW5oIFThur8gUXXhu5FjIETDom4gKE5FVSk!5e1!3m2!1svi!2s!4v1788366465649!5m2!1svi!2s",
   musicUrl: "",
   dressCode: ["BLUE", "WHITE", "JEANS"],
   bring: "Một tinh thần thật vui và sẵn sàng làm quen!",
